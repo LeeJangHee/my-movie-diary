e73baeff8d643e2d0822ba6d3eaa2c694e057e96
JangHee96 <sj90947@gmail.com>
2022-01-25 03:38:53 +0900

    - Bottom Choice category 스와이프 뷰 추가 - "취소" 버튼은 항상 레이아웃 마지막에 표시 될 수 있도록 수정해야함

--- app/src/main/java/com/devlee/mymoviediary/presentation/fragment/main_bottom/create/ChoiceBottomSheet.kt
+++ app/src/main/java/com/devlee/mymoviediary/presentation/fragment/main_bottom/create/ChoiceBottomSheet.kt
@@ -1,18 +1,25 @@
 package com.devlee.mymoviediary.presentation.fragment.main_bottom.create
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.devlee.mymoviediary.R
 import com.devlee.mymoviediary.databinding.BottomChoiceViewBinding
 import com.devlee.mymoviediary.presentation.adapter.create.CreateBottomSheetAdapter
 import com.devlee.mymoviediary.viewmodels.ContentCreateViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ChoiceBottomSheet : BottomSheetDialogFragment() {
+
+    private val TAG = "ChoiceBottomSheet"
 
     private var _binding: BottomChoiceViewBinding? = null
     private val binding get() = _binding!!
@@ -38,8 +45,41 @@ class ChoiceBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated: ")
+
+        if (args.bottomChoiceType == BottomChoiceType.CATEGORY) {
+            val layoutParams = (binding.root.parent as View).layoutParams as CoordinatorLayout.LayoutParams
+            val behavior = layoutParams.behavior
+            if (behavior is BottomSheetBehavior) {
+                behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                    override fun onStateChanged(bottomSheet: View, newState: Int) {
+                        when (newState) {
+                            BottomSheetBehavior.STATE_HIDDEN -> {
+                                Log.d(TAG, "onStateChanged: STATE_HIDDEN")
+                                dismissAllowingStateLoss()
+                            }
+                            BottomSheetBehavior.STATE_COLLAPSED -> Log.d(TAG, "onStateChanged: STATE_COLLAPSED")
+                            BottomSheetBehavior.STATE_DRAGGING -> Log.d(TAG, "onStateChanged: STATE_DRAGGING")
+                            BottomSheetBehavior.STATE_EXPANDED -> Log.d(TAG, "onStateChanged: STATE_EXPANDED")
+                            BottomSheetBehavior.STATE_SETTLING -> Log.d(TAG, "onStateChanged: STATE_SETTLING")
+                        }
+                    }
+
+                    override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+
+                })
+                behavior.peekHeight = (requireContext().resources.getDimension(R.dimen.dp_56) * 5).toInt()
+            }
+        }
+
         choiceBottomSheetAdapter.setBottomSheetItem(args.choiceBottomSheetList.toList())
         setRecyclerView()
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun setupDialog(dialog: Dialog, style: Int) {
+        super.setupDialog(dialog, style)
+        Log.d(TAG, "setupDialog: ")
     }
 
     override fun onResume() {
--- app/src/main/java/com/devlee/mymoviediary/presentation/layout/RoundDialog.kt
+++ app/src/main/java/com/devlee/mymoviediary/presentation/layout/RoundDialog.kt
@@ -1,11 +1,16 @@
 package com.devlee.mymoviediary.presentation.layout
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.devlee.mymoviediary.R
+import com.devlee.mymoviediary.utils.getColorRes
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 
 class RoundDialog @JvmOverloads constructor(
     context: Context,
@@ -15,14 +20,20 @@ class RoundDialog @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyle) {
 
     private var cornerRadius: Int
+    private var cornerRadiusTop: Int
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.RoundDialog, defStyleAttr, defStyle).apply {
             try {
                 cornerRadius = getDimensionPixelSize(R.styleable.RoundDialog_cornerRadius, 0)
+                cornerRadiusTop = getDimensionPixelSize(R.styleable.RoundDialog_cornerRadiusTop, 0)
 
                 if (cornerRadius > 0) {
                     setCornerRadius()
+                }
+
+                if (cornerRadiusTop > 0) {
+                    setCornerRadiusTop()
                 }
             } finally {
                 recycle()
@@ -40,5 +51,19 @@ class RoundDialog @JvmOverloads constructor(
         }
         setBackground(gradientDrawable)
         clipToOutline = true
+    }
+
+    private fun setCornerRadiusTop() {
+        clipToOutline = true
+        val model = ShapeAppearanceModel().toBuilder().apply {
+            setTopRightCorner(CornerFamily.ROUNDED, cornerRadiusTop.toFloat())
+            setTopLeftCorner(CornerFamily.ROUNDED, cornerRadiusTop.toFloat())
+        }.build()
+
+        val shape = MaterialShapeDrawable(model).apply {
+            val backgroundColor = getColorRes(context, R.color.white)
+            fillColor = ColorStateList.valueOf(backgroundColor)
+        }
+        background = shape
     }
 }
